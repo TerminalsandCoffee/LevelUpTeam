6baabfe7607d1d40d299f711bf3acf646af946fe
Rafael Martinez <rgmllc@yahoo.com>
2023-02-02 22:01:15 +0000

    mergerd dev branch into main

--- adduser.sh
+++ adduser.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
-echo "Enter the name of the new user:"
-read username
-# check if the user already exists
-if id "$username" >/dev/null 2>&1; then
-  echo "User $username already exists."
-  exit 1
-fi
-echo "Adding user $username to the system..."
-# create the user account
-useradd -m $username
-# set the password for the new user
-echo "Enter a password for the new user:"
-passwd $username
-echo "User $username has been added successfully."
+
+# function to add a user
+add_user() {
+  username=$1
+  # check if the user already exists
+  if id "$username" >/dev/null 2>&1; then
+    echo "User $username already exists."
+    return 1
+  fi
+  echo "Adding user $username to the system..."
+  # create the user account
+  useradd -m $username
+  # set the password for the new user
+  echo "Enter a password for the new user:"
+  passwd $username
+  echo "User $username has been added successfully."
+}
+
+# loop through all the arguments
+for i in "$@"; do
+  add_user $i
+done
